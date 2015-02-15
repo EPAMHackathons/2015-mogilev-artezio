52b543ed4b256a3ab4a7727ae725dce9817aa67b
GrigoryCherkasov <grigory.cherkasov@gmail.com>
2015-02-15 14:31:27 +0300

    enums
    enums

---
+++ top-weather-web/src/main/java/weather/model/weather/enumeration/Overcast.java
@@ -0,0 +1,18 @@
+package weather.model.weather.enumeration;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gcherkasov
+ * Date: 20.11.14
+ * Time: 10:37
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Overcast {
+    OVC, //Сплошная облачность
+    BKN, //Облачно с прояснениями
+    SCT, //Переменная облачность
+    FEW, //Небольшая облачность
+    NSC, //Несущественная облачность
+    SKC, //Ясно
+    NONE // Неизвестно
+}
---
+++ top-weather-web/src/main/java/weather/model/weather/enumeration/PeriodOfDay.java
@@ -0,0 +1,11 @@
+package weather.model.weather.enumeration;
+
+/**
+ * Created by Grigory on 31.12.2014.
+ */
+public enum PeriodOfDay {
+    MORNING,
+    DAY,
+    EVENING,
+    NIGHT
+}
