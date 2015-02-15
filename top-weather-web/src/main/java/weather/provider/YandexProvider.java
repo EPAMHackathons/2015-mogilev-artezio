335eae020108aa20d11d1760a52b1f4b75448d7b
OsipencoV <olya250282@mail.ru>
2015-02-15 12:07:45 +0300

    yandex provider

---
+++ top-weather-web/src/main/java/weather/provider/YandexProvider.java
@@ -0,0 +1,16 @@
+package weather.provider;
+
+import weather.model.Request;
+
+import java.util.List;
+
+/**
+ * @author vosipenko
+ *   on 15.02.2015.
+ */
+public class YandexProvider implements WeatherForecastProvider {
+    @Override
+    public List<Request> getWeather(String providerUrl) {
+        return null;
+    }
+}
