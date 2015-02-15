504b86d88e4dd1479aa945b89bdb8d44373bcdbc
a-markevich <AlexeYM007@gmail.com>
2015-02-15 13:38:28 +0300

    TopWeatherBean

--- top-weather-web/src/main/java/weather/beans/TopWeatherBean.java
+++ top-weather-web/src/main/java/weather/beans/TopWeatherBean.java
@@ -2,10 +2,50 @@ package weather.beans;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import weather.dto.RateTableDto;
+import weather.dto.RequestDto;
+import weather.model.Request;
+import weather.model.enumeration.FeatureType;
+import weather.model.enumeration.OrderType;
+import weather.model.enumeration.Period;
+import weather.service.*;
+import weather.util.Pair;
+import weather.util.Utils;
 
 import java.util.List;
 
 @Component
 public class TopWeatherBean {
+
+    @Autowired
+    private RequestService requestService;
+
+    @Autowired
+    private ForecastService forecastService;
+
+
+    /**
+     * Request forecast for period(day/week...)
+     * @param locationUID
+     * @param providerId
+     * @param period
+     * @return forecast for period
+     */
+    public List<RequestDto> getForecastForPeriod(String locationUID, Long providerId, Period period) {
+        List<Request> requests = requestService.getForecastForPeriod(locationUID, providerId, period);
+        List<RequestDto> requestsDto = Utils.requestConverter(requests);
+        return requestsDto;
+    }
+
+    /**
+     *  Main rate table
+     * @param locationUID
+     * @param orderField
+     * @param orderType
+     * @return rate table
+     */
+    public RateTableDto getForecast(String locationUID, Pair<Period, FeatureType> orderField, OrderType orderType) {
+        return forecastService.getForecast(locationUID, orderField, orderType);
+    }
 
 }
--- top-weather-web/src/main/java/weather/util/Utils.java
+++ top-weather-web/src/main/java/weather/util/Utils.java
@@ -2,6 +2,8 @@ package weather.util;
 
 
 import org.apache.commons.lang.time.DateUtils;
+import weather.dto.RequestDto;
+import weather.model.Request;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -14,6 +16,22 @@ public class Utils {
 
     public static Date formatEndDate(Date date) {
         return DateUtils.addMilliseconds(DateUtils.ceiling(date, Calendar.DATE), -1);
+    }
+
+    public static List<RequestDto> requestConverter(List<Request> requests) {
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("EE", Locale.getDefault());
+        List<RequestDto> requestsDto = new ArrayList<RequestDto>(requests.size());
+        for (Request request : requests) {
+            RequestDto requestDto = new RequestDto();
+            requestDto.setForecastDate(request.getForecastDate());
+            requestDto.setRequestDate(request.getRequestDate());
+            requestDto.setForecastDayOfWeek(simpleDateFormat.format(request.getForecastDate()));
+            requestDto.setRequestDayOfWeek(simpleDateFormat.format(request.getRequestDate()));
+            requestDto.setForecasts(request.getForecasts());
+            requestDto.setRequestRule(request.getRequestRule());
+            requestsDto.add(requestDto);
+        }
+        return requestsDto;
     }
 
 }
